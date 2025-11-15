8a153e933fdce0064f001f3bcf651c9cf4a44e7c
Elena Seiler <ele.seiler.24@lehre.mosbach.dhbw.de>
2025-11-15 19:35:41 +0100

    Grundgerüst mit Header und Logout

---
+++ Client/src/views/studentenplanerView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header class="header">
+      <h1>Studentenplaner</h1>
+      <div class="user-info">
+        <span v-if="authStore.user">Hallo, {{ authStore.user.username }}!</span>
+        <button @click="handleLogout" class="logout-btn">
+          Ausloggen
+        </button>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/authStore'
+
+const authStore = useAuthStore()
+
+function handleLogout() {
+  authStore.logout()
+}
+</script>
